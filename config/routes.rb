bc473a7235577596bc6dc70798d7427b70fb966b
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-05-15 16:45:46 -0700

    Add Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'static_pages#root'
+
+  namespace :api, defaults: {format: :json} do
+    resources :user, only: [:create]
+    resource :session, only: [:create, :destroy]
+    post '/podcasts/', to: 'podcasts#search', as: "podcasts"
+  end
 end
