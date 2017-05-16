f663d80437c49434376f7c27d858e5d77706dbe6
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-05-15 17:03:02 -0700

    Setup NPM, JS, and React

---
+++ frontend/entry.jsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootEl = document.getElementById('root');
+  ReactDOM.render(<h1>React is kinda working</h1>, rootEl);
+});
