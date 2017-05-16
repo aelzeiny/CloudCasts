1e00ed2cd755b7b9e6ac7a29a73693ac5387e7e2
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-05-16 12:04:54 -0700

    Basic Navbar Component Setup

--- frontend/components/navbar/navbar_container.jsx
+++ frontend/components/navbar/navbar_container.jsx
@@ -9,28 +9,31 @@ class NavbarComponent extends React.Component {
 
   render() {
     return (
-      <nav className="navbar navbar-toggleable-md navbar-light bg-faded">
+      <nav className="navbar navbar-toggleable-md fixed-top navbar-light bg-faded">
         <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">
+          CommuteCasts <i className="fa fa-headphones"></i>
+        </a>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="#">
+                Discover
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#">Disabled</a>
+              <a className="nav-link" href="#">
+                Listen
+              </a>
             </li>
           </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
+          <ul className="navbar-nav">
+            <i className="fa fa-user"></i> LOG IN
+            <i className="fa fa-user"></i> SIGN UP
+          </ul>
         </div>
       </nav>
     );
