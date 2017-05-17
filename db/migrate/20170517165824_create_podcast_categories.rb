d049dd77b085ab27d274274c3d89004db5f09d0b
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-05-17 10:05:08 -0700

    Categories Join Table

---
+++ db/migrate/20170517165824_create_podcast_categories.rb
@@ -0,0 +1,10 @@
+class CreatePodcastCategories < ActiveRecord::Migration[5.0]
+  def change
+    create_table :podcast_categories do |t|
+      t.integer :podcast_id
+      t.integer :category_id
+      
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517164152) do
+ActiveRecord::Schema.define(version: 20170517165824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20170517164152) do
     t.text     "description", null: false
     t.integer  "length",      null: false
     t.datetime "release",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "podcast_categories", force: :cascade do |t|
+    t.integer  "podcast_id"
+    t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
