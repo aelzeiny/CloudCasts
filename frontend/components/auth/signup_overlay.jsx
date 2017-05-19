19dd8b0291d38f4e8a4f3d16699c3fca9031d92b
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-05-19 11:51:54 -0700

    Signup Overlay

---
+++ frontend/components/auth/signup_overlay.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default LoginOverlay = function(props) {
+  return (
+    <div className="overlay">
+      <form onSubmit={this.props.onSubmit}>
+        <div className="form-group">
+          <label htmlFor="un">Username</label>
+          <input className="form-control" type="text" name="username" id="un"></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="pw">Password</label>
+          <input className="form-control" type="password" name="password" id="pw"></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="cpw">Confirm Password</label>
+          <input className="form-control" type="password" name="password_confirm" id="cpw"></input>
+        </div>
+        <button class="btn btn-danger">submit</button>
+      </form>
+    </div>
+  );
+}
