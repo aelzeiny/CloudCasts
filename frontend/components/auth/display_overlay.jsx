79fc00654dd0bf757393c7d28c331c55a67bc6d6
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-05-19 11:34:34 -0700

    Display Overlay

---
+++ frontend/components/auth/display_overlay.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default DisplayOverlay = function(props) {
+  return (
+    <div className="overlay">
+      <i className="fa fa-headphones fa-6"></i>
+    </div>
+  )
+}
