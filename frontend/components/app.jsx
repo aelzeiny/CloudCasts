34485657d6c2a478af14e6bc4b039470a15fedc0
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-05-19 11:15:53 -0700

    Init basic route at homescreen

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
-import {LoginFormContainer} from './modals/login_form';
+import {AuthComponent} from './auth/auth_component';
 import {SignupFormContainer} from './modals/signup_form';
 
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
@@ -11,12 +11,9 @@ import DiscoverContainer from './discover/discover_component';
 
 const App = () => (
   <div>
-    <header>
-      <Route path="/" component={NavBarContainer} />
-    </header>
     <Switch>
-      <ProtectedRoute path="/podcasts" compone/>
-      <AuthRoute path="/auth" component={HeroComponent}/>
+      <AuthRoute path="/login" component={AuthComponent} />
+      <ProtectedRoute path="/listen" component={ListenComponent} />
     </Switch>
   </div>
 );
