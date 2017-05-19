324be4c3c70b2fb36d7ad51a079123a126036217
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-05-19 12:00:37 -0700

    Fix exporting syntax errors

--- frontend/components/auth/display_overlay.jsx
+++ frontend/components/auth/display_overlay.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default DisplayOverlay = function(props) {
+export default function(props) {
   return (
     <div className="overlay">
       <i className="fa fa-headphones fa-6"></i>
--- frontend/components/auth/login_overlay.jsx
+++ frontend/components/auth/login_overlay.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default LoginOverlay = function(props) {
+export default function(props) {
   return (
     <div className="overlay">
       <form onSubmit={this.props.onSubmit}>
