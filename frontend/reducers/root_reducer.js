e3b213fb0e902a3084a153443cfc39bdc436a7bf
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-05-23 11:51:24 -0700

    Update Root Reducer with episode slice of state

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,14 +1,18 @@
 import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import PodcastsReducer from './podcasts_reducer';
+import EpisodeReducer from './episode_reducer';
 import SubscriptionReducer from './subscription_reducer';
 import PodcastReducer from './podcast_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,
+
+  episode: EpisodeReducer,
+  podcast: PodcastReducer,
   podcasts: PodcastsReducer,
-  subscriptions: SubscriptionReducer,
-  podcast: PodcastReducer
+
+  subscriptions: SubscriptionReducer
 });
 
 export default RootReducer;
