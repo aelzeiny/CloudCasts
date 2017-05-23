2cbd220bf647ef818bbbb32a6d0ad4d361195cac
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-05-23 15:56:40 -0700

    remove player controls, begin custom styling

--- frontend/components/listen/player/player_container.jsx
+++ frontend/components/listen/player/player_container.jsx
@@ -22,10 +22,26 @@ class PlayerContainer extends React.Component {
     return (
       <footer className="player">
         <div className="container">
-          <audio ref={(me) => this.player = me} controls>
+          <audio ref={(me) => this.player = me}>
+
+            {/* <!-- Optional Source --> */}
             {this._getSrc()}
+
+            {/* <!--Warning --> */}
             Your browser does not support our audio player
           </audio>
+        </div>
+
+        {/* <!-- Video Controls --> */}
+        <div id="video-controls">
+          <button type="button" id="play-pause">
+            <i className="fa fa-play"></i>
+          </button>
+          <input type="range" id="seek-bar" value="0"/>
+          <button type="button" id="mute">
+            <i className="fa fa-pause"></i>
+          </button>
+          <input type="range" id="volume-bar" min="0" max="1" step="0.1" value="1"/>
         </div>
       </footer>
     );
