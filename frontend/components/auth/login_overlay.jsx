537e3704903b8d77981760cb1a83154f7ee5c1bb
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-05-22 23:22:59 -0700

    Add Google Material Text Inputs

--- frontend/components/auth/login_overlay.jsx
+++ frontend/components/auth/login_overlay.jsx
@@ -5,17 +5,26 @@ export default function(props) {
     <div className="overlay center">
       <form id="loginForm" onSubmit={props.onSubmit}>
         <div className="form-group">
-          <label htmlFor="un">Username</label>
-          <input className="form-control" type="text" name="username" id="un"></input>
+          <div className="group">
+            <input type="text" name="username" id="un" required/>
+            <span className="highlight"></span>
+            <span className="bar"></span>
+            <label htmlFor="un">Username</label>
+          </div>
         </div>
         <div className="form-group">
-          <label htmlFor="pw">Password</label>
-          <input className="form-control" type="password" name="password" id="pw"></input>
+          <div className="group">
+            <input type="password" name="password" id="pw" required/>
+            <span className="highlight"></span>
+            <span className="bar"></span>
+            <label htmlFor="pw">Password</label>
+          </div>
         </div>
         <button className="btn btn-outline-danger btn-lg">log in</button>
         <button type="button" name="signup" className="btn btn-link btn-outline-link" onClick={props.onOptionChange}>
           signup instead
         </button>
+
       </form>
     </div>
   );
