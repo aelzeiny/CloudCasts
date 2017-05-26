5125b6453707cec1a5c85ac23b5e9ff678de0629
--add <--add>
2017-05-26 04:46:12 -0700

    Push to github and heroku

--- frontend/components/auth/logo_overlay.jsx
+++ frontend/components/auth/logo_overlay.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 export default function(props) {
   return (
     <div className="overlay" id="logo">
-      <img src="headphones.png"></img>
+      <a href="\">
+        <img src="headphones.png"></img>
+      </a>
     </div>
   );
 }
