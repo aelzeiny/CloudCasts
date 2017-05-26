3a8ce2a14bc75ff6dc7cfe696a9961229f797340
Ahmed Elzeiny <ahmed.elzeiny@gmail.com>
2017-05-26 07:29:27 -0700

    Fix user icon displacement

--- frontend/components/auth/logo_overlay.jsx
+++ frontend/components/auth/logo_overlay.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function(props) {
   return (
     <div className="overlay" id="logo">
-      <a href="\">
+      <a href="#">
         <img src="headphones.png"></img>
       </a>
     </div>
